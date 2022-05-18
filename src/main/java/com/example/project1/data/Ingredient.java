4b4224242f019255dab8f1aec89a2f479cf70169
Daniel Gaumond <danielgaumond@hotmail.com>
2022-05-18 08:09:35 -0400

    Initial commit

---
+++ src/main/java/com/example/project1/data/Ingredient.java
@@ -0,0 +1,16 @@
+package com.example.project1.data;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+
+}
