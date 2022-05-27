b8da2ac3bed3d28e346aad8997706f35df2ad8f7
Daniel Gaumond <danielgaumond@hotmail.com>
2022-05-26 23:06:19 -0400

    Add on

--- src/main/java/com/example/project1/data/Ingredient.java
+++ src/main/java/com/example/project1/data/Ingredient.java
@@ -1,17 +1,27 @@
 package com.example.project1.data;
 
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-@Data
-@Table
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@RequiredArgsConstructor
+//@AllArgsConstructor
+///*
+//  @NoArgsConstructor will generate a constructor with no parameters. If this is not possible (because of final fields), a compiler error will result instead, unless @NoArgsConstructor(force = true) is used, then all final fields are initialized with 0 / false / null.
+// */
+//@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
 public class Ingredient {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+
     private final String id;
     private final String name;
     private final Type type;
@@ -20,19 +30,32 @@ public class Ingredient {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
 
-    @Bean
-    public ApplicationRunner dataLoader(@Autowired IngredientRepository repo) {
-        return args -> {
-            repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
-            repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-            repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-            repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
-            repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-            repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
-            repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-            repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
-            repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
-        };
+//    @Bean
+//    public ApplicationRunner dataLoader(@Autowired IngredientRepository repo) {
+//        return args -> {
+//            repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+//            repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+//            repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+//            repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+//            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+//            repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+//            repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+//            repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+//            repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+//            repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+//        };
+//    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Ingredient that = (Ingredient) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
