a750ce73526306c6c15e6bdc75e0dd3f4d4da993
Daniel Gaumond <danielgaumond@hotmail.com>
2022-05-27 00:25:10 -0400

    Towards Spring Data JPA

--- src/main/java/com/example/project1/data/repositories/OrderRepository.java
+++ src/main/java/com/example/project1/data/repositories/OrderRepository.java
@@ -2,7 +2,21 @@
 package com.example.project1.data.repositories;
 
 import com.example.project1.data.TacoOrder;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(
+            String deliveryZip, Date startDate, Date endDate);
+    List<TacoOrder> findByDeliveryToAndDeliveryCityAllIgnoresCase(
+            String deliveryTo, String deliveryCity);
+    @Query(value = "SELECT o FROM TacoOrder o where o.deliveryCity='Seattle'")
+    List<TacoOrder> readOrdersDeliveredInSeattle();
+
+    @Query("SELECT t FROM TacoOrder t WHERE t.deliveryCity = ?1")
+    TacoOrder findTacoOrderByDeliveryCity(String deliveryCity);
 }
